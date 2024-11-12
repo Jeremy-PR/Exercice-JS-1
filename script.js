8f0ab372b635601e88bf287ef1b6df4f0ca97bb9
Jeremy-PR <jeremy.pelisse@gmail.com>
2024-11-12 16:54:36 +0100

    début des exercices

---
+++ script.js
@@ -0,0 +1,23 @@
+// // exercice 1 
+// console.log("Hello, world !");
+// console.log("Bienvenue dans le monde de la programmation !");
+
+
+
+
+// exercice 2
+
+// let nombre1 = 5
+// let nombre2 = 10
+// console.log(nombre1 + nombre2);
+
+
+
+
+// exercice 3
+
+
+
+
+
+
