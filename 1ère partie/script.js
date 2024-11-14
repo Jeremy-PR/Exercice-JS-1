28ab5a6db11683db66f8b959837b59111e026251
Jeremy-PR <jeremy.pelisse@gmail.com>
2024-11-14 11:01:08 +0100

    correction 1ere partie

--- 1ère partie/script.js
+++ 1ère partie/script.js
@@ -1,28 +1,46 @@
-// // exercice 1 
+// // exercice 1
 // console.log("Hello, world !");
 // console.log("Bienvenue dans le monde de la programmation !");
 
 
 
 
+
+
 // exercice 2
 
-// let nombre1 = 5
-// let nombre2 = 10
+// const nombre1 = 5
+// const nombre2 = 10
 // console.log(nombre1 + nombre2);
+
+
+
 
 
 
 
 // // exercice 3
 
-// let fruits = ["pomme", "banane", "cerise"]
+// const fruits = ["pomme", "banane", "cerise"]
 
 //  fruits.forEach((fruits, index) =>{
 //     console.log(fruits, index);
-    
+
 //  })
- 
+
+//  correction exercice 3 :
+
+//  const fruits = ["pomme", "banane", "cerise"]
+
+// fruits.forEach((fruit) => {
+//     console.log(fruit);
+// })
+
+
+
+
+
+
 
 // exercice 4
 
@@ -36,27 +54,106 @@
 
 
 
+
+
+
+
+
+// correction exercice 4  :
+
+for (let count = 1; count <= 20; count += 1) {
+  if (count % 2 === 0) {
+    console.log(count);
+  }
+}
+
+
+
+
+
+
+
 // //   exercice 5
 
 // const mot = "garagiste";
 
-
 // let voyelles = 0;
 
-
 // for (let i = 0; mot[i]; i++) {
-   
+
 //     if ("aeiou".includes(mot[i])) {
 //         voyelles++;
 //     }
 // }
-
 // console.log("Le nombre de voyelles dans le mot est :", voyelles);
+
+
+
+
+
+// correction exercice 5
+
+// function countVowels(word) {
+//   const vowels = "aeiouyAEIOUY";
+//   let count = 0;
+
+//   for (let char of word) {
+//     if (vowels.includes(char)) {
+//       count += 1;
+//     }
+//   }
+//   return count;
+// }
+//  console.log(countVowels("number of Vowels"));
+
+
+
+
+// 2eme correction exo 5
+
+// function countVowels(word){
+//     const matches = word.match(/[aeiouy]/gi);
+
+//     //si matches a une valeur
+//     if(matches){
+//         return matches.length; // car matche retourne un tableau
+//     }
+// }
+
+// const word = "Nomer of vowels";
+// console.log("Number of vowels in " + word + "," + countVowels(word));
+
+
+
+
+
+
 
 
 // // exercice 6
 
 // const phrase = "js c'est pas facile"
+
+
+// correction exo 6
+
+// function countLetter(sentence) {
+//   const letterCounts = {};
+//   for (let letter of sentence) {
+//     if (letter.match(/[a-z]/gi)) {
+//       letter = letter.toLowerCase();
+//       if (letterCounts[letter]) {
+//         letterCounts[letter] += 1;
+//       } else {
+//         letterCounts[letter] = 1;
+//       }
+//     }
+//   }
+//   return letterCounts;
+// }
+// const randomSentence = "Une phrase random !";
+// countLetters(randomSentence);
+
 
 
 
@@ -73,16 +170,36 @@
 
 // for ( let i = 0; i < chiffres.length; i++){
 //     if (chiffres[i] >= 0 ){
-//       positif.push(chiffres[i]);  
-//     } 
+//       positif.push(chiffres[i]);
+//     }
 //     else {negatif.push(chiffres[i])};
 
 // }
-    
-
 
 // console.log(positif);
 // console.log(negatif);
+
+
+// correction exercice 7
+
+// const numbers = [3, -1, 2, -7];
+// const positiveNumbers = [];
+// const negativeNumbers = [];
+
+// numbers.forEach((number) => {
+//     if(number >= 0){
+//         positiveNumbers.push(number);
+//     } else {
+//         negativeNumbers.push(number);
+//     }
+// })
+
+// console.log("Nombres positifs : ", positiveNumbers);
+// console.log("Nombres negatifs : ", negativeNumbers);
+
+
+
+
 
 
 
@@ -101,18 +218,37 @@
 
 
 
+// correction exercice 8
+
+// const numbers = [3, 7, 2, 9, 5];
+// let maxNumber = numbers[0];
+
+// for (let i = 1; i < numbers.length; i += 1) {
+//   if (numbers[i] > maxNumber) {
+//     maxNumber = numbers[i];
+//   }
+// }
+
+// console.log(maxNumber);
+
+
+
 // // exercice 9
 
 // let word = "marsupilami"
 
 // let inverse = word.split("").reverse("").join("");
 
-
 // console.log("Le mot inversé est : " + inverse)
 
 
+// correction exercice 9
 
+// // const word = "bonjour";
+// // let reversedWord = "";
 
+// // for (let i = word.length - 1; i >= 0; i -= 1) {
+// //     reversedWord += word[i];
+// // }
 
-
-
+// // console.log(reversedWord);
